6e4d8924dbb73f0e45a30481f108fa4303873c4b
Rocio <23300657@uttt.edu.mx>
2026-03-01 23:48:45 -0600

    actualización de funciones

--- config/initializers/healthcheck.rb
+++ config/initializers/healthcheck.rb
@@ -1,5 +1,12 @@
 # config/initializers/healthcheck.rb
 Rails.application.routes.prepend do
   get "healthcheck", to: proc { [200, {}, ["OK"]] }
-  get "up", to: proc { [200, {}, ["OK"]] }  # Railway usa /up por defecto
+  get "up", to: proc { [200, {}, ["OK"]] }
+  get "db-check", to: proc { 
+    begin
+      ActiveRecord::Base.connection.active? ? [200, {}, ["DB OK"]] : [500, {}, ["DB Failed"]]
+    rescue => e
+      [500, {}, ["DB Error: #{e.message}"]]
+    end
+  }
 end
